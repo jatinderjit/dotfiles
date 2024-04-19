bb61a628da3ca519c68b615d904f7805eebef7e5
Jatinderjit Singh <jatinderjit89@gmail.com>
2024-04-19 12:37:24 +0530

    Install utilities using cargo

--- setups/ubuntu_server.sh
+++ setups/ubuntu_server.sh
@@ -6,6 +6,13 @@ SERVER_NAME=
 # read -p "Enter server name: " SERVER_NAME
 # read -p "SSH keys: enter github usernames: " GITHUB_USERNAMES
 
+ZSH_PLUGINS=''
+add_zsh_plugin () {
+    ZSH_PLUGINS="$ZSH_PLUGINS\n  $1"
+}
+add_zsh_plugin git
+add_zsh_plugin pip
+
 # Add SSH Keys
 GITHUB_USERNAMES=(jatinderjit) # space separated usernames
 for username in $GITHUB_USERNAMES; do
@@ -15,7 +22,12 @@ done
 
 sudo apt update
 sudo apt upgrade
+
 sudo apt install -y zsh git gettext zip unzip
+sudo apt install -y build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev curl \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 
 # ZSH #########################################################################
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
@@ -35,64 +47,154 @@ sed -i "s/SERVER_NAME/$SERVER_NAME/" ~/.oh-my-zsh/custom/themes/custom.zsh-theme
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="custom"/' ~/.zshrc
 
 echo 'export LANG=en_US.UTF-8' >> ~/.zshrc
-echo 'export EDITOR=vim' >> ~/.zshrc
+echo 'export EDITOR=nvim' >> ~/.zshrc
+echo '' >> ~/.zshrc
+
 echo "alias gl='git log'" >> ~/.zshrc
 echo "alias glp='git log --patch'" >> ~/.zshrc
+echo "alias gap='git add --patch'" >> ~/.zshrc
+echo "alias gdc='git diff --cached'" >> ~/.zshrc
+echo "unalias gco" >> ~/.zshrc
+echo '' >> ~/.zshrc
+
+echo "alias vim=nvim" >> ~/.zshrc
 echo "alias v=vim" >> ~/.zshrc
 echo "alias vd="vim -d"" >> ~/.zshrc
+echo '' >> ~/.zshrc
+
+echo "alias rgi='rg -i'" >> ~/.zshrc
+echo "alias rgn='rg --no-ignore'" >> ~/.zshrc
+echo "alias rgf='rg --files-with-matches'" >> ~/.zshrc
 echo "alias -g G='| rg'" >> ~/.zshrc  # global alias (substitute anywhere in a command)
 echo "alias -g Gi='| rg -i'" >> ~/.zshrc  # global alias (substitute anywhere in a command)
+echo "alias fdi='fd -i'" >> ~/.zshrc
+echo "alias fdn='fd --no-ignore'" >> ~/.zshrc
 echo "alias -s git='git clone'" >> ~/.zshrc
-echo "alias ls=exa" >> ~/.zshrc
-echo "alias cat="bat --theme=1337"" >> ~/.zshrc
+echo "alias ls='eza --icons'" >> ~/.zshrc
+echo "alias cat='bat --theme=1337'" >> ~/.zshrc
 echo "export MANPAGER=\"sh -c 'col -bx | bat --theme=Dracula -l man -p'\"" >> ~/.zshrc
-echo "alias rgn='rg --no-ignore'" >> ~/.zshrc  # to ignore .gitignore (and other ignore files)
-echo 'alias ls=exa' >> ~/.zshrc
-echo 'alias fd=fdfind' >> ~/.zshrc
-echo 'alias py=python' >> ~/.zshrc
-echo 'alias j=just' >> ~/.zshrc
+echo "alias fd=fdfind" >> ~/.zshrc
+echo "alias py=python" >> ~/.zshrc
+echo "alias j=just" >> ~/.zshrc
 
 
 # Utilities ###################################################################
 
-# install bat, dust, sd, jq, choose, just
-download_install() {
-    # download_install name url
+download_extract() {
+    url=$1
     pushd /tmp
-    wget $2
-    if [[ $2 == *.tar.gz ]]; then
-        fn=$(echo $2 | choose -f '/' -1)
-        dir_name=$(echo $fn | choose -f '.tar.gz' -1)
-        tar -xf $fn
-        mv "$dir_name/$1" .
-        rm -r $dir_name $fn
-    elif [[ $2 == *choose* ]]; then
-        mv choose-x86_64-unknown-linux-gnu choose
-    else
-        mv "$(echo $2 | choose -f '/' -1)" $1
-    fi
-    sudo chmod +x $1
-    sudo mv $1 /usr/local/bin/
+    wget $url
+    fn=$(echo $url | choose -f '/' 1)
+    dir_name=$(echo $fn | choose -f '.tar.gz' -1)
     popd
 }
 
-sudo apt install -y ripgrep exa fd-find
-download_install choose https://github.com/theryangeary/choose/releases/download/v1.3.4/choose-x86_64-unknown-linux-gnu
-download_install bat https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-x86_64-unknown-linux-gnu.tar.gz
-download_install dust https://github.com/bootandy/dust/releases/download/v0.8.6/dust-v0.8.6-x86_64-unknown-linux-gnu.tar.gz
-download_install sd https://github.com/chmln/sd/releases/download/v0.7.6/sd-v0.7.6-x86_64-unknown-linux-gnu
-download_install jq https://github.com/jqlang/jq/releases/download/jq-1.6/jq-linux64
+# install bat, dust, sd, jq, choose, just
+download_install() {
+    # download_install name url
+    bin_name=$1
+    bin_url=$2
+    pushd /tmp
+    wget $bin_url
+    if [[ $bin_url == *.tar.gz ]]; then
+        fn=$(echo $bin_url | choose -f '/' -1)
+        dir_name=$(echo $fn | choose -f '.tar.gz' -1)
+        tar -xf $fn
+        mv "$dir_name/$bin_name" .
+        rm -r $dir_name $fn
+    elif [[ $bin_url == *choose* ]]; then
+        mv choose-x86_64-unknown-linux-gnu choose
+    else
+        mv "$(echo $bin_url | choose -f '/' -1)" $1
+    fi
+    sudo chmod +x $bin_name
+    sudo mv $bin_name /usr/local/bin/
+    popd
+}
 
-wget https://github.com/casey/just/releases/download/1.14.0/just-1.14.0-x86_64-unknown-linux-musl.tar.gz
-tar -xf just-1.14.0-x86_64-unknown-linux-musl.tar.gz && rm just-1.14.0-x86_64-unknown-linux-musl.tar.gz && rm just.1
+download_install choose https://github.com/theryangeary/choose/releases/latest/download/choose-x86_64-unknown-linux-gnu
+
+if [ $(which nvim) ]; then
+    echo "Already installed: nvim"
+else
+    download_extract https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+    sudo mv nvim-linux64 /opt/nvim
+    rm nvim-linux64.tar.gz
+    echo '' >> ~/.zshrc
+    echo 'export PATH="/opt/nvim/bin:$PATH"' >> ~/.zshrc
+    echo "Installed: nvim"
+fi
+
+if [ $(which cargo) ]; then
+    echo "Already installed: rust"
+else
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    source ~/.zshrc
+    echo "Installed: rust"
+fi
+
+if [ $(which rg) ]; then
+    echo "Already installed: ripgrep"
+else
+    cargo install ripgrep
+    echo "Installed: ripgrep"
+fi
+add_zsh_plugin ripgrep  # Always add
+
+if [ $(which eza) ]; then
+    echo "Already installed: eza"
+else
+    cargo install eza
+    echo "Installed: eza"
+fi
+add_zsh_plugin eza
+
+if [ $(which fd) ]; then
+    echo "Already installed: fd"
+else
+    cargo install fd-find
+    echo "Installed: fd"
+fi
+add_zsh_plugin fd
+
+if [ $(which bat) ]; then
+    echo "Already installed: bat"
+else
+    cargo install bat
+    echo "Installed: bat"
+fi
+
+if [ $(which sd) ]; then
+    echo "Already installed: sd"
+else
+    cargo install sd
+    echo "Installed: sd"
+fi
+
+if [ $(which dust) ]; then
+    echo "Already installed: dust"
+else
+    cargo install du-dust
+    echo "Installed: dust"
+fi
+
+
+if [ $(which jq) ]; then
+    echo "Already installed: jq"
+else
+    download_install jq https://github.com/jqlang/jq/releases/latest/download/jq-linux-amd64
+    echo "Installed: jq"
+fi
+
+wget https://github.com/casey/just/releases/download/1.25.2/just-1.25.2-x86_64-unknown-linux-musl.tar.gz
+tar -xf just-1.25.2-x86_64-unknown-linux-musl.tar.gz && rm just-1.25.2-x86_64-unknown-linux-musl.tar.gz && rm just.1
 sudo chmod +x just && sudo mv just /usr/local/bin
 mkdir $ZSH/custom/plugins/just
 just --completions zsh >> $ZSH/custom/plugins/just/_just
-sd 'plugins=\(git\)' 'plugins=(git just)' ~/.zshrc
-omz reload
+add_zsh_plugin just
 
 # Vim #########################################################################
-sudo wget https://gist.githubusercontent.com/jatinderjit/f51b7cb01ec7bca4297b0f9f782e4eb1/raw/d026fb2c7cf57f5a2104b468f23c7de283297935/vimrc -O /etc/vim/vimrc.local
+sudo wget https://gist.githubusercontent.com/jatinderjit/f51b7cb01ec7bca4297b0f9f782e4eb1/raw/ba46fae8241a7a43c54672037fc07bb4f7b0427e/vimrc -O /etc/vim/vimrc.local
 
 # Certbot ###################################################################
 # sudo apt-get install -y snapd
@@ -126,31 +228,36 @@ echo '' >> ~/.zshrc
 echo 'export PYENV_ROOT="/opt/pyenv"' >> ~/.zshrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-omz reload
+export PYENV_ROOT=/opt/pyenv
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
-omz reload
-
-sudo apt install -y build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev curl \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 # Poetry ################
-curl -sSL https://install.python-poetry.org | sudo POETRY_HOME=/opt/poetry python3 -
-sudo chown -R $(whoami):$(whoami) /opt/poetry
-echo 'export PATH=/opt/poetry/bin:$PATH' >> ~/.zshrc
-echo ''
-omz reload
+if [ $(which poetry) ]; then
+    echo "Already installed: poetry"
+else
+    curl -sSL https://install.python-poetry.org | sudo POETRY_HOME=/opt/poetry python3 -
+    sudo chown -R $(whoami):$(whoami) /opt/poetry
+    echo 'export PATH=/opt/poetry/bin:$PATH' >> ~/.zshrc
+    echo ''
+    echo "Installed: poetry"
+fi
 
 # Go ##########################################################################
-cd /tmp/
-wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
-tar -xf go1.21.5.linux-amd64.tar.gz
-sudo mv go /opt/go-1.21.5
-sudo ln -s /opt/go-1.21.5 /opt/go
-echo 'export PATH=/opt/go/bin:$PATH' >> ~/.zshrc
-omz reload
+if [ $(which go) ]; then
+    echo "Already installed: go"
+else
+    cd /tmp/
+    wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
+    tar -xf go1.22.0.linux-amd64.tar.gz
+    sudo mv go /opt/go-1.22.0
+    sudo ln -s /opt/go-1.22.0 /opt/go
+    echo 'export PATH=/opt/go/bin:$PATH' >> ~/.zshrc
+    echo "Installed: go"
+fi
 
 # Redis #######################################################################
 sudo apt install redis-server
@@ -161,3 +268,15 @@ sudo systemctl disable redis
 sudo apt install postgresql postgis libpq-dev
 sudo systemctl stop postgresql
 sudo systemctl disable postgresql
+
+# oh my zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+add_zsh_plugin zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+add_zsh_plugin zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+add_zsh_plugin zsh-history-substring-search
+
+sd 'plugins=\(git\)' "plugins=($(echo $ZSH_PLUGINS)\n)" ~/.zshrc
