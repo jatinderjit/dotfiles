48f5ebb460d6ad50ab32ae10a10e48434580019d
Jatinderjit Singh <jatinderjit89@gmail.com>
2025-04-27 23:31:31 +0530

    Re-enable rerere

--- kenv/scripts/dark-mode.ts
+++ kenv/scripts/dark-mode.ts
@@ -9,5 +9,5 @@ import { usePowerShell } from 'zx'
 
 usePowerShell()
 
-await $`New-ItemProperty -Path HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force` // Get the host information
-await $`New-ItemProperty -Path HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force` // Get the host information
+await $`New-ItemProperty -Path HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force`
+await $`New-ItemProperty -Path HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force`
--- kenv/scripts/light-mode.ts
+++ kenv/scripts/light-mode.ts
@@ -9,6 +9,5 @@ import { usePowerShell } from 'zx'
 
 usePowerShell()
 
-await $`New-ItemProperty -Path HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -Name AppsUseLightTheme -Value 1 -Type Dword -Force` // Get the host information
-await $`New-ItemProperty -Path HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -Name SystemUsesLightTheme -Value 1 -Type Dword -Force` // Get the host information
-
+await $`New-ItemProperty -Path HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -Name AppsUseLightTheme -Value 1 -Type Dword -Force`
+await $`New-ItemProperty -Path HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -Name SystemUsesLightTheme -Value 1 -Type Dword -Force`
